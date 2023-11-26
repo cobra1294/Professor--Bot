b1b634e0b1ee38d80b3068599f4661c94e424602
cobra1294 <139133937+cobra1294@users.noreply.github.com>
2023-11-26 05:33:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -57,7 +57,7 @@ if len(SUPPORT_GROUP) == 0:
 else:
     SUPPORT_GROUP = int(SUPPORT_GROUP)
     
-OPENAI_API = environ.get('OPENAI_API', '')
+OPENAI_API = environ.get('OPENAI_API', '0')
 if len(OPENAI_API) == 0:
     logging.error('OPENAI_API is missing, exiting now')
     exit()
