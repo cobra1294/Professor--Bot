efca234b92e14d831140805b532cfb25f425b2ed
cobra1294 <139133937+cobra1294@users.noreply.github.com>
2023-12-28 15:54:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,7 +106,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://professor-gva.onrender.com")
+URL = environ.get("URL", "https://t.me/rqsturmovie1/8539")
 if len(URL) == 0:
     logging.error('URL is missing, exiting now')
     exit()
