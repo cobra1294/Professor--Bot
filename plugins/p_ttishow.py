ec27c3a1ab415720de457249a1f3f4e6d42e47f1
cobra1294 <139133937+cobra1294@users.noreply.github.com>
2024-02-04 19:47:05 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1,6 +1,6 @@
 import random, os, sys
 from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ChatMemberUpdated
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import ADMINS, LOG_CHANNEL, PICS, SUPPORT_LINK, UPDATES_LINK
 from database.users_chats_db import db
@@ -9,19 +9,19 @@ from utils import get_size, temp, get_settings
 from Script import script
 from pyrogram.errors import ChatAdminRequired
 
+
 @Client.on_chat_member_updated(filters.group)
-async def welcome(bot, message: ChatMemberUpdated):
+async def welcome(bot, message):
     if message.new_chat_member and not message.old_chat_member:
         if message.new_chat_member.user.id == temp.ME:
             buttons = [[
-                InlineKeyboardButton('Updates Channel', url=UPDATES_LINK),
-                InlineKeyboardButton('Support Group', url=SUPPORT_LINK)
+                InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇ', url=UPDATES_LINK),
+                InlineKeyboardButton('ꜱᴜᴘᴘᴏʀᴛ', url=SUPPORT_LINK)
             ]]
             reply_markup=InlineKeyboardMarkup(buttons)
             user = message.from_user.mention if message.from_user else "Dear"
             await bot.send_photo(chat_id=message.chat.id, photo=random.choice(PICS), caption=f"👋 Hello {user},\n\nThank you for adding me to the <b>'{message.chat.title}'</b> group, Don't forget to make me admin. If you want to know more ask the support group. 😘</b>", reply_markup=reply_markup)
             return
-            
         settings = await get_settings(message.chat.id)
         if settings["welcome"]:
             WELCOME = settings['welcome_text']
@@ -38,7 +38,6 @@ async def restart_bot(bot, message):
     with open('restart.txt', 'w+') as file:
         file.write(f"{msg.chat.id}\n{msg.id}")
     os.execl(sys.executable, sys.executable, "bot.py")
-
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
@@ -65,9 +64,8 @@ async def leave_a_chat(bot, message):
             text=f'Hello Friends,\nMy owner has told me to leave from group so i go! If you need add me again contact my support group.\nReason - <code>{reason}</code>',
             reply_markup=reply_markup,
         )
-
         await bot.leave_chat(chat)
-        await message.reply(f"Left the chat `{chat}`")
+        await message.reply(f"<b>✅️ Successfully bot left from this group - `{chat}`</b>")
     except Exception as e:
         await message.reply(f'Error - {e}')
 
@@ -107,7 +105,6 @@ async def disable_chat(bot, message):
     except Exception as e:
         await message.reply(f"Error - {e}")
 
-
 @Client.on_message(filters.command('unban_grp') & filters.user(ADMINS))
 async def re_enable_chat(bot, message):
     if len(message.command) == 1:
@@ -126,7 +123,6 @@ async def re_enable_chat(bot, message):
     temp.BANNED_CHATS.remove(int(chat_))
     await message.reply("Chat successfully re-enabled")
 
-
 @Client.on_message(filters.command('invite_link') & filters.user(ADMINS))
 async def gen_invite_link(bot, message):
     if len(message.command) == 1:
@@ -139,7 +135,7 @@ async def gen_invite_link(bot, message):
     try:
         link = await bot.create_chat_invite_link(chat)
     except Exception as e:
-        return await message.reply(f'Error {e}')
+        return await message.reply(f'Error - {e}')
     await message.reply(f'Here is your invite link: {link.invite_link}')
 
 @Client.on_message(filters.command('ban_user') & filters.user(ADMINS))
@@ -170,9 +166,7 @@ async def ban_a_user(bot, message):
         await db.ban_user(k.id, reason)
         temp.BANNED_USERS.append(k.id)
         await message.reply(f"Successfully banned {k.mention}")
-
-
-    
+   
 @Client.on_message(filters.command('unban_user') & filters.user(ADMINS))
 async def unban_a_user(bot, message):
     if len(message.command) == 1:
@@ -197,8 +191,6 @@ async def unban_a_user(bot, message):
         await db.remove_ban(k.id)
         temp.BANNED_USERS.remove(k.id)
         await message.reply(f"Successfully unbanned {k.mention}")
-
-
     
 @Client.on_message(filters.command('users') & filters.user(ADMINS))
 async def list_users(bot, message):
@@ -206,16 +198,20 @@ async def list_users(bot, message):
     users = await db.get_all_users()
     out = "Users saved in database are:\n\n"
     async for user in users:
-        out += f"**Name:** {user['name']}\n**ID:** `{user['id']}`\n"
+        out += f"**Name:** {user['name']}\n**ID:** `{user['id']}`"
         if user['ban_status']['is_banned']:
-            out += '(Banned User)'
-        out += '\n'
+            out += ' (Banned User)'
+        if user['verify_status']['is_verified']:
+            out += ' (Verified User)'
+        out += '\n\n'
     try:
         await raju.edit_text(out)
     except MessageTooLong:
         with open('users.txt', 'w+') as outfile:
             outfile.write(out)
         await message.reply_document('users.txt', caption="List of users")
+        await raju.delete()
+        os.remove('users.txt')
 
 @Client.on_message(filters.command('chats') & filters.user(ADMINS))
 async def list_chats(bot, message):
@@ -223,13 +219,16 @@ async def list_chats(bot, message):
     chats = await db.get_all_chats()
     out = "Chats saved in database are:\n\n"
     async for chat in chats:
-        out += f"**Title:** {chat['title']}\n**ID:** `{chat['id']}`\n"
+        out += f"**Title:** {chat['title']}\n**ID:** `{chat['id']}`"
         if chat['chat_status']['is_disabled']:
-            out += '(Disabled Chat)'
-        out += '\n'
+            out += ' (Disabled Chat)'
+        out += '\n\n'
     try:
         await raju.edit_text(out)
     except MessageTooLong:
         with open('chats.txt', 'w+') as outfile:
             outfile.write(out)
         await message.reply_document('chats.txt', caption="List of chats")
+        await raju.delete()
+        os.remove('chats.txt')
+        
