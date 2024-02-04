288e80f6cfd3025253be827219fa8e5de557dcba
cobra1294 <139133937+cobra1294@users.noreply.github.com>
2024-02-04 13:22:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,24 @@
-import logging
 from pyrogram import Client, __version__
-import pyromod.listen
-from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from aiohttp import web
 from database.users_chats_db import db
-from web import web_server
+from web import web_app
 from info import LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
-import time, os
+import time, os, platform
 from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
 
 
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name='Auto_Filter_Bot',
+            name='PROFESSOR',
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
+            plugins={"root": "plugins"}
         )
 
     async def start(self):
@@ -47,27 +42,62 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         username = '@' + me.username
-        app = web.AppRunner(await web_server())
+        app = web.AppRunner(web_app)
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
-        logging.info(f"\n\nBot [{username}] Started!\n\n")
         try:
             await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
         except:
-            logging.error("Make sure bot admin in LOG_CHANNEL, exiting now")
+            print("Error - Make sure bot admin in LOG_CHANNEL, exiting now")
             exit()
         try:
             m = await self.send_message(chat_id=BIN_CHANNEL, text="Test")
             await m.delete()
         except:
-            logging.error("Make sure bot admin in BIN_CHANNEL, exiting now")
+            print("Error - Make sure bot admin in BIN_CHANNEL, exiting now")
             exit()
+        print(f"\nPyrogram [v{__version__}] Bot [{username}] Started With Python [v{platform.python_version()}]\n")
 
 
     async def stop(self, *args):
         await super().stop()
-        logging.info("Bot stopped! Bye...")
+        print("Bot Stopped! Bye...")
 
+
+    async def iter_messages(self: Client, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
+        """Iterate through a chat sequentially.
+        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
+        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
+        single call.
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book you can use his phone number (str).
+                
+            limit (``int``):
+                Identifier of the last message to be returned.
+                
+            offset (``int``, *optional*):
+                Identifier of the first message to be returned.
+                Defaults to 0.
+        Returns:
+            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+        Example:
+            .. code-block:: python
+                async for message in app.iter_messages("pyrogram", 1000, 100):
+                    print(message.text)
+        """
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
 
 app = Bot()
-app.run()        
+app.run()
+                
