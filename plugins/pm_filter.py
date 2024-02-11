ec7de62bf8aad63c82f46c9e5872cf30a1de1a24
cobra1294 <139133937+cobra1294@users.noreply.github.com>
2024-02-11 07:14:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -416,7 +416,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data.startswith("stream"):
          file_id = query.data.split('#', 1)[1]
-         try:
             user_id = query.from_user.id
             msg = await client.send_cached_media(chat_id=BIN_CHANNEL, file_id=file_id)
             watch = f"{URL}watch/{msg.id}"
