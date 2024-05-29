a23b7c79b3626e970fbe41b9c7ea5ad5a441499e
ℜø𝔩ë𝔵 𓃵 <139133937+cobra1294@users.noreply.github.com>
2024-05-29 21:36:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-import re, logging
+import re
 from os import environ
 from Script import script
 
@@ -48,16 +48,12 @@ else:
 INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1001719548150').split()]
 if len(INDEX_CHANNELS) == 0:
     print('Info - INDEX_CHANNELS is empty')
-AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '0').split()]
-if len(AUTH_CHANNEL) == 0:
-    print('Info - AUTH_CHANNEL is empty')
 LOG_CHANNEL = environ.get('LOG_CHANNEL', '')
 if len(LOG_CHANNEL) == 0:
     print('Error - LOG_CHANNEL is missing, exiting now')
     exit()
 else:
     LOG_CHANNEL = int(LOG_CHANNEL)
-IS_FSUB = is_enabled('IS_FSUB', False)
     
 SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1001918925906')
 if len(SUPPORT_GROUP) == 0:
@@ -65,10 +61,6 @@ if len(SUPPORT_GROUP) == 0:
     exit()
 else:
     SUPPORT_GROUP = int(SUPPORT_GROUP)
-    
-OPENAI_API = environ.get('OPENAI_API', '0')
-if len(OPENAI_API) == 0:
-    print('Info - OPENAI_API is empty')
 
 # MongoDB information
 DATABASE_URL = environ.get('DATABASE_URL', "")
@@ -99,7 +91,8 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 MAX_BTN = int(environ.get('MAX_BTN', 10))
 PAYMENT_QR = environ.get('PAYMENT_QR', 'https://graph.org/file/46fd33d87206259d3071b.jpg')
 LANGUAGES = [language.lower() for language in environ.get('LANGUAGES', 'tam hin tel kan mal eng multi dual').split()]
-PM_DELETE_TIME = int(environ.get('PM_DELETE_TIME', 1200))
+QUALITY = [quality.lower() for quality in environ.get('QUALITY', '360p 480p 720p 1080p 2160p').split()]
+PM_FILE_DELETE_TIME = int(environ.get('PM_FILE_DELETE_TIME', 1200))
 
 # Other
 IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', True)
@@ -113,6 +106,10 @@ WELCOME_TEXT = environ.get("WELCOME_TEXT", script.WELCOME_TEXT)
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/how_to_openlinkz/19")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/how_to_openlinkz/19")
 INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTENSIONS', 'mp4 mkv').split()]
+
+#premium info
+PAYMENT_QR = environ.get('PAYMENT_QR', 'https://telegra.ph/Payment-Information-02-03-5')
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'p.satiz004@oksbi')
 
 #start_command_reactions
 REACTIONS = ["😇", "🤗", "😍", "👍", "🥰", "😱", "😘", "⚡️"]
